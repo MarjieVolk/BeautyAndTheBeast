3c17a3134315b7f4f7e3cb9e99a5c39c4905e7a9
Marjie Volk <marjie.volk@comcast.net>
2012-12-13 22:12:41 -0600

    Added song assets and a semi-randomized music player

--- UnityProject/Assets/Code/Core/MusicController.cs
+++ UnityProject/Assets/Code/Core/MusicController.cs
@@ -1,12 +1,42 @@
 using System;
+using UnityEngine;
 
-namespace AssemblyCSharp
-{
-	public class MusicController
-	{
-		public MusicController ()
-		{
+public class MusicController : MonoBehaviour {
+	
+	public AudioClip[] clips;
+	
+	private static readonly int MIN_WAIT_TIME = 10;
+	private static readonly int MAX_WAIT_TIME = 180;
+	
+	private System.Random r;
+	private float timeOfLastSong = -1;
+		
+	void Start() {
+		r = new System.Random();
+	}
+	
+	void Update() {
+		if (audio.isPlaying) {
+			timeOfLastSong = Time.time;
+		} else {
+			if (startSong()) {
+				audio.clip = getSong();
+				audio.Play();
+			}
 		}
+	}
+	
+	private AudioClip getSong() {
+		return clips[r.Next(clips.Length)];
+	}
+	
+	private bool startSong() {
+		if (timeOfLastSong == -1)
+			return true;
+		
+		float diff = Time.time - timeOfLastSong;
+		double probability = (diff - MIN_WAIT_TIME) / (MAX_WAIT_TIME - MIN_WAIT_TIME);
+		return r.NextDouble() < probability;
 	}
 }
 
